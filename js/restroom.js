f38cdc808c4da2f4c1be75eaef54fd44054db326
jreboqui@ucsd.edu <jreboqui@ucsd.edu>
2018-10-30 00:19:09 -0700

    Moved jquery code to restroom.js

---
+++ js/restroom.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+    $("#plus_preferences").click(function(){
+        $("#bathpreference").slideToggle();
+    });
+    $("#plus_building").click(function(){
+        $("#buildingPreference").slideToggle();
+    });
+
+});
