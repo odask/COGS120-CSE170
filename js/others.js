f9a049c99426f8423fd3d2fe5c89ac137c729ba2
Junnel Reboquio <jreboqui@ucsd.edu>
2018-10-31 23:08:20 -0700

    Added contents, css and js for other_categories.html

---
+++ js/others.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    
+    
+    var place_selected = false;
+    
+    $("#study_rooms").click(function(){
+        if (place_selected == false){
+            $(".subMenu").slideToggle();
+            place_selected = true;
+        }
+        else {
+            $(".subMenu").slideToggle();
+            place_selected = false;
+            $(".room_preferences1, .room_preferences2, #search1").css("display", "none");
+          
+        }
+        
+    });
+
+    $("#food").click(function(){
+        if (place_selected == false){
+            $(".foodSubMenu").slideToggle();
+            place_selected = true;
+        }
+        else {
+            $(".foodSubMenu").slideToggle();
+            place_selected = false;
+            $(".food_pc, #search1").css("display", "none");
+          
+        }
+        
+    });
+
+
+
+    $("input[name=building]:radio").click(function () {
+        if ($('input[name=building]:checked').val() == "geisel") {
+            $('.room_preferences2').hide();
+            $('.room_preferences1').show();
+
+        } else if ($('input[name=building]:checked').val() == "pc") {
+            $('.room_preferences2').show();
+            $('.room_preferences1').hide();
+
+        } else if ($('input[name=building]:checked').val() == "pc_food") {
+            $('.food_pc').show();
+        } 
+
+
+    });
+
+    $("input[name=room]:radio").click(function() {
+        if($('input[name=room]:checked').val() == "205" || $('input[name=room]:checked').val() == "206" ||
+        $('input[name=room]:checked').val() == "bk"){
+          $('#search1').show();
+        }
+    });
+
+});
