6d5b4b9b0f89f7af939274fe064de9ce388b24ac
Junnel Reboquio <jreboqui@ucsd.edu>
2018-11-01 22:04:42 -0700

    Updated jquery so it changes when collapsing/expanding

--- js/restroom.js
+++ js/restroom.js
@@ -1,11 +1,34 @@
  $(document).ready(function(){
     
-    
+    var pref_expanded = false;
+    var building_expanded = false;
     $("#plus_preferences").click(function(){
-        $("#bathpreference").slideToggle();
+        if (!pref_expanded){
+            $("#bathpreference").slideToggle();
+            pref_expanded = true;
+            $('#plus_preferences').attr('src', 'icons/minus-button.png');
+        }
+        else {
+            $("#bathpreference").slideToggle();
+            pref_expanded = false;
+            $('#plus_preferences').attr('src', 'icons/add-plus-button.png');
+            
+
+        }
+       
     });
     $("#plus_building").click(function(){
-        $("#buildingPreference").slideToggle();
+        if (!pref_expanded){
+            $("#buildingPreference").slideToggle();
+            pref_expanded = true;
+            $('#plus_building').attr('src', 'icons/minus-button.png');
+
+        }
+        else {
+            $("#buildingPreference").slideToggle();
+            pref_expanded = false;
+            $('#plus_building').attr('src', 'icons/add-plus-button.png');
+        }
     });
 
 });
