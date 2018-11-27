4ca2e235debef734b878ebc614cd17f1525e5609
Junnel Reboquio <jreboqui@ucsd.edu>
2018-11-26 16:25:49 -0800

    removed user and password check

--- js/index2.js
+++ js/index2.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
     //On click of login, get the username and password
+    //FOR NOW, ANY EMAIL AND PASSWORD WILL BE ACCEPTED AND LOG
+    //THE USER IN
     $("#submitBtn").click(function(){
        console.log("Login button clicked");
 
        var email = document.getElementById("email").value;
        var password = document.getElementById("password").value;
-    
-       if (email == 'admin@ucsd.edu' && password == "password"){
-           console.log("Let him in");
-           window.localStorage.setItem('user', email);
-           window.localStorage.setItem('password', password);
-           window.localStorage.setItem('guest', false);
-           window.location.href='index.html';
-       }
-       else{
-           //Toast Message here
-           //Plus username and password borders to red
+       window.localStorage.setItem('guest', false);
+       window.location.href='index.html';
 
-       }
+    
+    //    if (email == 'admin@ucsd.edu' && password == "password"){
+    //        console.log("Let him in");
+    //        window.localStorage.setItem('user', email);
+    //        window.localStorage.setItem('password', password);
+    //        window.localStorage.setItem('guest', false);
+    //        window.location.href='index.html';
+    //    }
+    //    else{
+    //        //Toast Message here
+    //        //Plus username and password borders to red
+
+    //    }
 
 
     });
